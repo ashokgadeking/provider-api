7a133b2586fcb3ba486b567ac9b58943a8052ff8
Ashok Teja Gadepalli <ashok.gadepalli@contino.io>
2021-01-31 19:04:04 -0600

    Testing printing commit sha

--- server/consumerTests/verifyPacts.js
+++ server/consumerTests/verifyPacts.js
@@ -3,6 +3,10 @@
 const pact = require('@pact-foundation/pact-node')
 require('./testProductsService')
 
+console.log("Printing GIT_COMMIT");
+
+console.log(process.env.GIT_COMMIT);
+
 const opts = {
   providerBaseUrl: 'http://localhost:3001', // where your service will be running during the test, either staging or localhost on CI
   providerStatesSetupUrl: 'http://localhost:3001/test/setup', // the url to call to set up states
