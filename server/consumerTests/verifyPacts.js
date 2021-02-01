9c2e6fd3c4e6d58c034950ac7d2a17fce6c2c8e9
Ashok Teja Gadepalli <ashok.gadepalli@contino.io>
2021-01-31 19:10:28 -0600

    Cleaning up

--- server/consumerTests/verifyPacts.js
+++ server/consumerTests/verifyPacts.js
@@ -3,16 +3,12 @@
 const pact = require('@pact-foundation/pact-node')
 require('./testProductsService')
 
-console.log("Printing GIT_COMMIT");
-
-console.log(process.env.GIT_COMMIT);
-
 const opts = {
   providerBaseUrl: 'http://localhost:3001', // where your service will be running during the test, either staging or localhost on CI
   providerStatesSetupUrl: 'http://localhost:3001/test/setup', // the url to call to set up states
   pactUrls: ['http://pact-broker:9292/pacts/provider/ProductService/consumer/Client/latest'], // the pacts to test against
   publishVerificationResult: true,
-  providerVersion: '1.0.0'.concat(process.env.GIT_COMMIT)
+  providerVersion: '1.0.0_'.concat(process.env.GIT_COMMIT)
 }
 
 pact.verifyPacts(opts).then(() => {
